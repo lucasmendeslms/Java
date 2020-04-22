cecf7112d001d5abb1760df392302e6fa9c8ec24
mendeslucas9 <57737521+mendeslucas9@users.noreply.github.com>
2020-04-21 21:53:57 -0300

    Estruturas repetitivas (while)

---
+++ Estruturas_condicionais/src/soma.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class soma {
+
+    public static void main(String[] args){
+
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Informe um número"));
+
+        int soma = 0;
+
+            while(num != 0){
+            soma += num;
+            num = Integer.parseInt(JOptionPane.showInputDialog("Informe um número"));
+            }
+
+        JOptionPane.showMessageDialog(null, "A soma dos valores informados é: " + soma);
+    }
+}
