f7947f3a41a6c587fa0c1fecdab24a9a07f8595d
mendeslucas9 <57737521+mendeslucas9@users.noreply.github.com>
2020-06-16 21:41:50 -0300

    Vetor
    Exercício sobre o uso de vetores.

---
+++ Comportamento de memória/Vetores/Vetor.java
@@ -0,0 +1,28 @@
+package Vetores;
+
+import javax.swing.JOptionPane;
+import java.util.Locale;
+
+public class Vetor {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        double soma = 0;
+
+        int n = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade de números a serem digitados."));
+
+        double[] vect = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            vect[i] = Double
+                    .parseDouble(JOptionPane.showInputDialog("Informe os números desejados para se calcular a média."));
+
+            soma += vect[i];
+        }
+
+        JOptionPane.showMessageDialog(null, "Valor da média entre os números informados = " + String.format("%.2f", soma/3));
+
+    }
+}
