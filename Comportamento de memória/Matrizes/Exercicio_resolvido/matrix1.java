a621f97e6e9742673169d50e0a69573062ce5e69
mendeslucas9 <57737521+mendeslucas9@users.noreply.github.com>
2020-07-02 23:46:12 -0300

    Update matrix1.java

--- Comportamento de memória/Matrizes/Exercicio_resolvido/matrix1.java
+++ Comportamento de memória/Matrizes/Exercicio_resolvido/matrix1.java
@@ -1,3 +1,7 @@
+/*Fazer um programa para ler um número inteiro N e uma matriz de
+ordem N contendo números inteiros. Em seguida, mostrar a diagonal
+principal e a quantidade de valores negativos da matriz.*/
+
 package Matrizes.Exercicio_resolvido;
 
 import javax.swing.JOptionPane;
